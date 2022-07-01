7f79b082778355525637e0704e3f59cfce571710
Your Name <you@example.com>
2022-06-30 21:53:56 -0300

    first commit

---
+++ x.py
@@ -0,0 +1,14 @@
+while(True):
+    nome = input("Digite o nome: ")
+    if nome == "0":
+        break
+    nota = int(input("Digite a nota: "))
+    if nota < 5:
+    #if  0 < nota <= 5:
+        nota = 6
+    
+    if nota > 10:
+        nota = 10
+    f = open('dados.txt', "a")
+    f.write(f"{nome};{nota}\n")
+    f.close()
